69f92a17ef80711232599bb3f49b210c8720906f
nkreu <nkreutzarek@hotmail.de>
2020-11-17 09:55:37 +0100

    Add REST fibonacci

---
+++ lab2/lab2-example/src/main/java/de/hska/iwi/vislab/lab2/example/FibonacciNumber.java
@@ -0,0 +1,53 @@
+package de.hska.iwi.vislab.lab2.example;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.prefs.*;
+
+@Path("fibonaccinumber")
+public class FibonacciNumber {
+
+    
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public int getIt() {
+        int n = this.readPreference();
+        int next = n + 1;
+        this.savePreference(next);
+        return computeFibonacci(n);
+    }
+
+    private int computeFibonacci(int n) {
+        if (n <= 2) {
+            return (n > 0) ? 1 : 0;
+        }
+        return computeFibonacci(n - 1) + computeFibonacci(n - 2);
+    }
+
+    private void savePreference(int n) {
+        Preferences prefs = Preferences.userNodeForPackage(FibonacciNumber.class);
+
+        prefs.putInt("number", n);
+    }
+
+    private int readPreference() {
+        Preferences prefs = Preferences.userNodeForPackage(FibonacciNumber.class);
+
+        return prefs.getInt("number", 0);
+    }
+
+    @DELETE
+    public void resetIt() {
+        this.savePreference(0);
+    }
+}
--- lab2/lab2-example/src/test/java/de/hska/iwi/vislab/lab2/example/HelloWorldTest.java
+++ lab2/lab2-example/src/test/java/de/hska/iwi/vislab/lab2/example/HelloWorldTest.java
@@ -1,9 +1,11 @@
 package de.hska.iwi.vislab.lab2.example;
 
+import javax.swing.text.html.parser.Entity;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
@@ -47,5 +49,27 @@ public class HelloWorldTest {
 	public void testSayHello() {
 		String responseMsg = target.path("helloworld").request().accept(MediaType.TEXT_PLAIN).get(String.class);
 		assertEquals("Hallo Welt!", responseMsg);
+	}
+
+	@Test
+	public void testFibonacciNumber() {
+		Response delResponse = target.path("fibonaccinumber").request().accept(MediaType.TEXT_PLAIN).delete();
+		assertEquals(204, delResponse.getStatus());
+		String response = target.path("fibonaccinumber").request().accept(MediaType.TEXT_PLAIN).post(null, String.class);
+		assertEquals(0, Integer.parseInt(response));
+
+		int max = 25;
+		int result = 1;
+		for (int i = 1; i <= max; i++) {
+			result = Integer.parseInt(target.path("fibonaccinumber").request().accept(MediaType.TEXT_PLAIN).post(null, String.class));
+			System.out.println(result);
+		}
+		// test the 25th f*-nr
+		assertEquals(75025, result);
+
+		delResponse = target.path("fibonaccinumber").request().accept(MediaType.TEXT_PLAIN).delete();
+		assertEquals(204, delResponse.getStatus());
+		response = target.path("fibonaccinumber").request().accept(MediaType.TEXT_PLAIN).post(null, String.class);
+		assertEquals(0, Integer.parseInt(response));
 	}
 }
